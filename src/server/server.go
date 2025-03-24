e2fa0d4af90486aad7f0c6f7f1ad56b42c4b1550
Nikhil Rao <nikhilcrao@gmail.com>
2025-03-24 10:26:59 +0530

    Add a ping handler

--- src/server/server.go
+++ src/server/server.go
@@ -1,14 +1,23 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 type Server struct {
 	router *gin.Engine
 }
 
 func NewServer(r *gin.Engine) (*Server, error) {
-	server := Server{router: r}
-	return &server, nil
+	s := Server{router: r}
+	s.setupRoutes()
+	return &s, nil
+}
+
+func (s *Server) setupRoutes() {
+	s.router.GET("/ping", func(ctx *gin.Context) { ctx.JSON(http.StatusOK, gin.H{"message": "pong"}) })
 }
 
 func (s *Server) Run(addr string) {
